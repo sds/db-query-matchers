9cd41eaa793c9fa12cd80a7d852045e5d2db19f7
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2020-07-08 09:31:26 -0700

    Use String matching operator (=~) in #pluralize

--- lib/db_query_matchers/make_database_queries.rb
+++ lib/db_query_matchers/make_database_queries.rb
@@ -35,7 +35,7 @@ RSpec::Matchers.define :make_database_queries do |options = {}|
 
   # Taken from ActionView::Helpers::TextHelper
   def pluralize(count, singular, plural = nil)
-    word = if count == 1 || count =~ /^1(\.0+)?$/
+    word = if count == 1 || count.to_s =~ /^1(\.0+)?$/
              singular
            else
              plural || singular.pluralize
